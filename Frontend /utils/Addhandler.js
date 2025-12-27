892383505b078c6d444ffde0ae6a6f3726e0f6e0
Suryansusm1234 <ssanjeebanimohanty@gmail.com>
2025-12-27 20:51:07 +0530

    Started the backend

--- Frontend /utils/Addhandler.js
+++ Frontend /utils/Addhandler.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
+import dayjs from 'dayjs';
 export async function addhandler({title, amount, desc, type}) {
   console.log({title ,amount,desc, type});
-  
-    const res = await axios.get("/api/hello")
-    console.log(res.data);
+    const res = await axios.post("/api/data", {
+      "title": title,
+      "amount": amount,
+      "desc": desc,
+      "type": type,
+      "createdAt": dayjs().format("YYYY-MM-DD HH:mm:ss")
+    })
   }
