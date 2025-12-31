682bd82a3364277d0ff38a76885dd2d4dac7ffe9
Suryansusm1234 <ssanjeebanimohanty@gmail.com>
2025-12-31 12:29:34 +0530

    refined

--- Frontend /components/Piechat.jsx
+++ Frontend /components/Piechat.jsx
@@ -1,7 +1,7 @@
 import { PieChart, Pie, Tooltip } from 'recharts';
 import TooltipContent from './TooltipContent';
 const Piechat = () => {
-  const data = [{ name: 'Bills and Utility', value: 15 , fill: '#0088FE'  }, { name: 'Food', value: 20, fill: '#00C49F'  }, { name: 'Personal', value: 30, fill: '#FFBB28' }, { name: 'HealthCare', value: 40, fill: '#FF8042' }, { name: 'Education', value: 50, fill: '#8884D8' }, { name: 'Transport', value: 60, fill: '#FF6B6B' }, { name: 'Investment', value: 70, fill: '#4ECDC4' }, { name: 'Other', value: 80, fill: '#92D1E3' }]
+  const data = [{ name: 'Bills and Utility', value: 15 , fill: '#06b6d4'  }, { name: 'Food', value: 20, fill: '#67e8f9'  }, { name: 'Personal', value: 30, fill: '#fef08a' }, { name: 'HealthCare', value: 40, fill: '#facc15' }, { name: 'Education', value: 50, fill: '#22c55e' }, { name: 'Transport', value: 60, fill: '#a5f3fc' }, { name: 'Investment', value: 70, fill: '#f9a8d4' }, { name: 'Other', value: 80, fill: '#ef4444' }]
   return (
     
     <div className="w-64 h-64">
