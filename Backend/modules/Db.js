4a81a32a54c89e6fe2098f93e926c44b6905bbd1
Suryansusm1234 <ssanjeebanimohanty@gmail.com>
2026-01-07 19:11:12 +0530

    Clean up

--- Backend/modules/Db.js
+++ Backend/modules/Db.js
@@ -3,9 +3,8 @@ import mongoose from 'mongoose'
 async function connectDB() {
     try {
         await mongoose.connect(process.env.MONGO_URI)
-        console.log("Connected");
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
 }
     
